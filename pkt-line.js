4d7432954db2d8205326d908b56109e74ac0365c
Tim Caswell <tim@creationix.com>
2013-10-14 16:25:45 -0500

    Support with and without sidechannel

--- pkt-line.js
+++ pkt-line.js
@@ -2,125 +2,178 @@
 var bops = require('bops');
 
 var PACK = bops.from("PACK");
-exports.deframer = function (emit) {
-  var state = 0;
-  var offset = 4;
-  var length = 0;
-  var data;
-  var fn = function (err, item) {
 
-    // Forward the EOS marker
-    if (item === undefined) return emit(err);
-
-    // Once we're in pack mode, everything goes straight through
-    if (state === 3) return emit(null, ["pack", item]);
-
-    // Otherwise parse the data using a state machine.
-    for (var i = 0, l = item.length; i < l; i++) {
-      var byte = item[i];
-      if (state === 0) {
-        var val = fromHexChar(byte);
-        if (val === -1) {
-          if (byte === PACK[0]) {
-            offset = 1;
-            state = 2;
-            continue;
-          }
-          state = -1;
-          return emit(new SyntaxError("Not a hex char: " + String.fromCharCode(byte)));
-        }
-        length |= val << ((--offset) * 4);
-        if (offset === 0) {
-          if (length === 4) {
-            offset = 4;
-            emit(null, ["line", ""]);
-          }
-          else if (length === 0) {
-            offset = 4;
-            emit(null, ["line", null]);
-          }
-          else if (length > 4) {
-            length -= 4;
-            data = bops.create(length);
-            state = 1;
-          }
-          else {
-            state = -1;
-            return emit(new SyntaxError("Invalid length: " + length));
-          }
-        }
-      }
-      else if (state === 1) {
-        data[offset++] = byte;
-        if (offset === length) {
-          offset = 4;
-          state = 0;
-          length = 0;
-          if (data[0] === 1) {
-            emit(null, ["pack", bops.subarray(data, 1)]);
-          }
-          else if (data[0] === 2) {
-            emit(null, ["progress", bops.to(bops.subarray(data, 1))]);
-          }
-          else if (data[0] === 3) {
-            emit(null, ["error", bops.to(bops.subarray(data, 1))]);
-          }
-          else {
-            emit(null, ["line", bops.to(data)]);
-          }
-        }
-      }
-      else if (state === 2) {
-        if (offset < 4 && byte === PACK[offset++]) {
-          continue;
-        }
-        state = 3;
-        emit(null, ["pack", bops.join([PACK, bops.subarray(item, i)])]);
-        break;
-      }
-      else {
-        emit(new Error("pkt-line decoder in invalid state"));
-      }
-    }
-  };
-  fn.is = "min-stream-write";
-  return fn;
+module.exports = {
+  deframer: deframer,
+  deframeMachine: deframeMachine,
+  framer: framer,
+  frame: frame
 };
-exports.deframer.is = "min-stream-push-filter";
 
-exports.framer = function (emit) {
-  var packMode = false;
-  var fn = function (err, item) {
-    if (item === undefined) return emit(err);
-    if (typeof item === "string") item = bops.from(item);
-    if (packMode) return emit(null, item);
-    if (item === null) return emit(null, bops.from("0000"));
-    if (item === true) {
-      packMode = true;
-      return;
+function deframer(emit) {
+  var state = deframeMachine(emit);
+  var working = false;
+
+  return function (chunk) {
+    if (working) throw new Error("parser is not re-entrant");
+    working = true;
+    // Once in raw mode, pass all data through.
+    if (!state) return emit("pack", chunk);
+
+    for (var i = 0, l = chunk.length; i < l; i++) {
+      state = state(chunk[i]);
+      if (state) continue;
+      emit("pack", bops.subarray(chunk, i));
+      break;
     }
-    emit(null, frameHead(item));
-    if (item.length) emit(null, item);
+    working = false;
   };
-  fn.is = "min-stream-write";
-  return fn;
-};
-exports.framer.is = "min-stream-push-filter";
-
-exports.frameHead = frameHead;
-function frameHead(item) {
-  var length = item.length + 4;
-  var buf = bops.create(4);
-  buf[0] = toHexChar(length >>> 12);
-  buf[1] = toHexChar((length >>> 8) & 0xf);
-  buf[2] = toHexChar((length >>> 4) & 0xf);
-  buf[3] = toHexChar(length & 0xf);
-  return buf;
 }
 
+function deframeMachine(emit) {
+  var offset, length, data, string, type;
+  return reset();
+
+  function reset() {
+    offset = 4;
+    length = 0;
+    data = null;
+    string = "";
+    type = undefined;
+    return $len;
+  }
+
+  function $len(byte) {
+    var val = fromHexChar(byte);
+    // If the byte isn't a hex char, it's bad input or it's raw PACK data.
+    if (val === -1) {
+      if (byte === PACK[0]) {
+        offset = 1;
+        return $maybeRaw;
+      }
+      throw new SyntaxError("Not a hex char: " + String.fromCharCode(byte));
+    }
+    length |= val << ((--offset) * 4);
+    if (offset) return $len;
+    if (length === 0) {
+      offset = 4;
+      emit("line", null);
+      return $len;
+    }
+    if (length === 4) {
+      offset = 4;
+      emit("line", "");
+      return $len;
+    }
+    if (length < 4) {
+      throw new SyntaxError("Invalid length: " + length);
+    }
+    length -= 4;
+    return $firstByte;
+  }
+
+  function $firstByte(byte) {
+    if (byte === 1) {
+      // PACK data in side-band
+      length--;
+      type = "pack";
+      data = bops.create(length);
+      return $binary;
+    }
+    if (byte === 2) {
+      length--;
+      type = "progress";
+      string = "";
+      return $string;
+    }
+    if (byte === 3) {
+      length--;
+      type = "error";
+      string = "";
+      return $string;
+    }
+    type = "line";
+    offset++;
+    string = String.fromCharCode(byte);
+    return $string;
+  }
+
+  function $binary(byte) {
+    data[offset] = byte;
+    if (++offset < length) return $binary;
+    emit(type, data);
+    return reset();
+  }
+
+  function $string(byte) {
+    string += String.fromCharCode(byte);
+    if (++offset < length) return $string;
+    emit(type, string);
+    return reset();
+  }
+
+  function $maybeRaw(byte) {
+    if (offset === 4) return emit("pack", PACK);
+    if (byte === PACK[offset++]) return $maybeRaw;
+    throw new SyntaxError("Invalid data in raw pack header");
+  }
+
+}
+
+function framer(emit) {
+  return function (type, value) {
+    emit(frame(type, value));
+  };
+}
+
+// Strings in line, progress, and error messages are assumed to be binary
+// encoded.  If you want to send unicode data, please utf8 encode first.
+function frame(type, value) {
+  // Allow raw binary to be passed through
+  if (value === undefined && bops.is(type)) return type;
+  // Frame everything else using pkg-line framing and sidechannel multiplexing.
+  var length, binary;
+  if (type === "line") {
+    if (value === null) return bops.from("0000");
+    value = bops.from(value);
+    length = value.length + 4;
+    binary = bops.create(length);
+    bops.copy(value, binary, 4);
+  }
+  else if (type === "pack") {
+    length = value.length + 5;
+    binary = bops.create(length);
+    binary[4] = 1;
+    bops.copy(value, binary, 5);
+  }
+  else if (type == "progress") {
+    value = bops.from(value);
+    length = value.length + 5;
+    binary = bops.create(length);
+    binary[4] = 2;
+    bops.copy(value, binary, 5);
+  }
+  else if (type == "error") {
+    value = bops.from(value);
+    length = value.length + 5;
+    binary = bops.create(length);
+    binary[4] = 3;
+    bops.copy(value, binary, 5);
+  }
+  else {
+    throw new Error("Unknown type: " + type);
+  }
+  binary[0] = toHexChar(length >> 12 & 0xf);
+  binary[1] = toHexChar(length >> 8 & 0xf);
+  binary[2] = toHexChar(length >> 4 & 0xf);
+  binary[3] = toHexChar(length & 0xf);
+  return binary;
+}
+
+// Given the code for a hex character, return it's value.
 function fromHexChar(val) {
-  return (val >= 0x30 && val <  0x40) ? val - 0x30 :
-        ((val >  0x60 && val <= 0x66) ? val - 0x57 : -1);
+  return val >= 0x30 && val <  0x40 ? val - 0x30 :
+         val >  0x60 && val <= 0x66 ? val - 0x57 : -1;
 }
 
 function toHexChar(val) {
--- test.js
+++ test.js
@@ -2,7 +2,6 @@
 
 var pktLine = require('./.');
 var bops = require('bops');
-var min = require('min-stream');
 var test = require('tape');
 
 // Some canned data manually broken up at weird places to test deframing logic
@@ -35,41 +34,85 @@ var messages = [
   ["line", "want 0000000000000000000000000000000000000000\n"],
   ["line", null],
   ["line", "hi\0ofs-delta hat party\n"],
-  ["pack", bops.from("PACKhelloworld")],
+  ["pack", bops.from("PACK")],
+  ["pack", bops.from("helloworld")],
   ["pack", bops.from("this is a long binary blob right? Right!")]
 ];
 
-var messages2 = [
-  "git-upload-pack /js-git\0host=localhost\0",
-  "52bc15b3e685afe74f8331bade5ed992b8aeedee HEAD\0multi_ack thin-pack side-band side-band-64k ofs-delta shallow no-progress include-tag multi_ack_detailed agent=git/1.8.1.2\n",
-  "52bc15b3e685afe74f8331bade5ed992b8aeedee refs/heads/master\n",
-  "52bc15b3e685afe74f8331bade5ed992b8aeedee refs/remotes/origin/HEAD\n",
-  "52bc15b3e685afe74f8331bade5ed992b8aeedee refs/remotes/origin/master\n",
-  null,  // pkt-flush events are encoded as null
-  "",    // So they can be distinguished from empty strings
-  "hi\n",
-  "want 0000000000000000000000000000000000000000\n",
-  null,
-  "hi\0ofs-delta hat party\n",
-  true,
-  bops.from("PACKhelloworld"),
-  bops.from("this is a long binary blob right? Right!")
-];
-
 var framed = [
-  "002b","git-upload-pack /js-git\0host=localhost\0",
-  "00ad","52bc15b3e685afe74f8331bade5ed992b8aeedee HEAD\0multi_ack thin-pack side-band side-band-64k ofs-delta shallow no-progress include-tag multi_ack_detailed agent=git/1.8.1.2\n",
-  "003f","52bc15b3e685afe74f8331bade5ed992b8aeedee refs/heads/master\n",
-  "0046","52bc15b3e685afe74f8331bade5ed992b8aeedee refs/remotes/origin/HEAD\n",
-  "0048","52bc15b3e685afe74f8331bade5ed992b8aeedee refs/remotes/origin/master\n",
+  "002bgit-upload-pack /js-git\0host=localhost\0",
+  "00ad52bc15b3e685afe74f8331bade5ed992b8aeedee HEAD\0multi_ack thin-pack side-band side-band-64k ofs-delta shallow no-progress include-tag multi_ack_detailed agent=git/1.8.1.2\n",
+  "003f52bc15b3e685afe74f8331bade5ed992b8aeedee refs/heads/master\n",
+  "004652bc15b3e685afe74f8331bade5ed992b8aeedee refs/remotes/origin/HEAD\n",
+  "004852bc15b3e685afe74f8331bade5ed992b8aeedee refs/remotes/origin/master\n",
   "0000",
   "0004",
-  "0007","hi\n",
-  "0032","want 0000000000000000000000000000000000000000\n",
+  "0007hi\n",
+  "0032want 0000000000000000000000000000000000000000\n",
   "0000",
-  "001b","hi\0ofs-delta hat party\n",
-  "PACKhelloworld",
+  "001bhi\0ofs-delta hat party\n",
+  "PACK", "helloworld",
   "this is a long binary blob right? Right!"
+];
+
+var chunked2 = [
+  '002bgit-upload-pack /js-git\u0000host=localhost',
+  '\u000000ad52bc15b3e685afe74f8331bade5ed992b8aee',
+  'dee HEAD\u0000multi_ack thin-pack side-band sid',
+  'e-band-64k ofs-delta shallow no-progress includ',
+  'e-tag multi_ack_detailed agent=git/1.8.1.2\n003',
+  'f52bc15b3e685afe74f8331bade5ed992b8aeedee refs/',
+  'heads/master\n004652bc15b3e685afe74f8331bade5ed',
+  '992b8aeedee refs/remotes/origin/HEAD\n004852bc1',
+  '5b3e685afe74f8331bade5ed992b8aeedee refs/remote',
+  's/origin/master\n000000040007hi\n0032want 00000',
+  '00000000000000000000000000000000000\n0000001bhi',
+  '\u0000ofs-delta hat party\n0013\u0001PACKhellow',
+  'orld0020\u0002This is a progress message\r0021',
+  '\u0002This is a progress message.\r0022\u0002Th',
+  'is is a progress message..\r0018\u0003There was',
+  ' an error\n002d\u0001this is a long binary blob',
+  ' right? Right!'
+];
+
+var messages2 = [
+  ["line", "git-upload-pack /js-git\0host=localhost\0"],
+  ["line", "52bc15b3e685afe74f8331bade5ed992b8aeedee HEAD\0multi_ack thin-pack side-band side-band-64k ofs-delta shallow no-progress include-tag multi_ack_detailed agent=git/1.8.1.2\n"],
+  ["line", "52bc15b3e685afe74f8331bade5ed992b8aeedee refs/heads/master\n"],
+  ["line", "52bc15b3e685afe74f8331bade5ed992b8aeedee refs/remotes/origin/HEAD\n"],
+  ["line", "52bc15b3e685afe74f8331bade5ed992b8aeedee refs/remotes/origin/master\n"],
+  ["line", null],  // pkt-flush events are encoded as null
+  ["line", ""],    // So they can be distinguished from empty strings
+  ["line", "hi\n"],
+  ["line", "want 0000000000000000000000000000000000000000\n"],
+  ["line", null],
+  ["line", "hi\0ofs-delta hat party\n"],
+  ["pack", bops.from("PACKhelloworld")],
+  ["progress", "This is a progress message\r"],
+  ["progress", "This is a progress message.\r"],
+  ["progress", "This is a progress message..\r"],
+  ["error", "There was an error\n"],
+  ["pack", bops.from("this is a long binary blob right? Right!")]
+];
+
+var framed2 = [
+  '002bgit-upload-pack /js-git\u0000host=localhost\u0000',
+  '00ad52bc15b3e685afe74f8331bade5ed992b8aeedee HEAD\u0000multi_ack thin-pack side-band side-band-64k ofs-delta shallow no-progress include-tag multi_ack_detailed agent=git/1.8.1.2\n',
+  '003f52bc15b3e685afe74f8331bade5ed992b8aeedee refs/heads/master\n',
+  '004652bc15b3e685afe74f8331bade5ed992b8aeedee refs/remotes/origin/HEAD\n',
+  '004852bc15b3e685afe74f8331bade5ed992b8aeedee refs/remotes/origin/master\n',
+  '0000',
+  '0004',
+  '0007hi\n',
+  '0032want 0000000000000000000000000000000000000000\n',
+  '0000',
+  '001bhi\u0000ofs-delta hat party\n',
+  '0013\u0001PACKhelloworld',
+  '0020\u0002This is a progress message\r',
+  '0021\u0002This is a progress message.\r',
+  '0022\u0002This is a progress message..\r',
+  '0018\u0003There was an error\n',
+  '002d\u0001this is a long binary blob right? Right!'
 ];
 
 
@@ -84,45 +127,81 @@ function toString(binary) {
   return bops.to(binary);
 }
 
-test('decoder works as expected', function(assert) {
+test('decoder works with no side-channel', function(assert) {
 
-  min.chain
-    .source(min.array(chunked.map(toBinary)))
-    .push(pktLine.deframer)
-    .sink(min.consume.sink(onDeframed));
-
-  function onDeframed(err, items) {
-    if (err) throw err;
-    items.forEach(function (item, i) {
-      if (bops.is(item[1])) {
-        assert.equal(item[0], messages[i][0]);
-        assert.equal(bops.to(item[1]), bops.to(messages[i][1]));
-      }
-      else {
-        assert.deepEqual(item, messages[i]);
-      }
-    });
-    assert.equal(items.length, messages.length);
-    assert.end();
-  }
+  var items = [];
+  chunked.map(toBinary).forEach(pktLine.deframer(function (type, value) {
+    items.push([type, value]);
+  }));
+  items.forEach(function (item, i) {
+    if (bops.is(item[1])) {
+      assert.equal(item[0], messages[i][0]);
+      assert.equal(bops.to(item[1]), bops.to(messages[i][1]));
+    }
+    else {
+      assert.deepEqual(item, messages[i]);
+    }
+  });
+  assert.equal(items.length, messages.length);
+  assert.end();
 
 });
 
-test('encoder works as expected', function(assert) {
+test('decoder works with side-channel', function(assert) {
 
-  min.chain
-    .source(min.array(messages2))
-    .push(pktLine.framer)
-    .sink(min.consume.sink(onFramed));
+  var items = [];
+  chunked2.map(toBinary).forEach(pktLine.deframer(function (type, value) {
+    items.push([type, value]);
+  }));
+  items.forEach(function (item, i) {
+    if (bops.is(item[1])) {
+      assert.equal(item[0], messages2[i][0]);
+      assert.equal(bops.to(item[1]), bops.to(messages2[i][1]));
+    }
+    else {
+      assert.deepEqual(item, messages2[i]);
+    }
+  });
+  assert.equal(items.length, messages2.length);
+  assert.end();
 
-  function onFramed(err, items) {
-    if (err) throw err;
-    items = items.map(toString);
-    items.forEach(function (item, i) {
-      assert.equal(item, framed[i]);
-    });
-    assert.equal(items.length, framed.length);
-    assert.end();
-  }
+});
+
+test('encoder works with no side-channel', function(assert) {
+
+  var items = [];
+  var write = pktLine.framer(function (item) {
+    items.push(item);
+  });
+  messages.forEach(function (pair) {
+    if (pair[0] === "pack") write(pair[1]);
+    else write(pair[0], pair[1]);
+  });
+
+  items = items.map(toString);
+  items.forEach(function (item, i) {
+    assert.equal(item, framed[i]);
+  });
+  assert.equal(items.length, framed.length);
+  assert.end();
+
+});
+
+test('encoder works with side-channel', function(assert) {
+
+  var items = [];
+  var write = pktLine.framer(function (item) {
+    items.push(item);
+  });
+  messages2.forEach(function (pair) {
+    write(pair[0], pair[1]);
+  });
+
+  items = items.map(toString);
+  items.forEach(function (item, i) {
+    assert.equal(item, framed2[i]);
+  });
+  assert.equal(items.length, framed2.length);
+  assert.end();
 
 });
